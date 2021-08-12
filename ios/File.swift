a5d8c8e683f90b0e489d9f58fe6984489ccf58ab
Ville Immonen <ville.immonen@iki.fi>
2021-08-12 15:40:27 +0300

    Configure dev client

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  RN065RC4
+//
+//  Created by Ville Immonen on 12.8.2021.
+//
+
+import Foundation
--- ios/RN065RC4/AppDelegate.m
+++ ios/RN065RC4/AppDelegate.m
@@ -1,8 +1,17 @@
 #import "AppDelegate.h"
 
+#if defined(EX_DEV_MENU_ENABLED)
+  @import EXDevMenu;
+#endif
+
+#if defined(EX_DEV_LAUNCHER_ENABLED)
+#include <EXDevLauncher/EXDevLauncherController.h>
+#endif
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -23,6 +32,12 @@ static void InitializeFlipper(UIApplication *application) {
 }
 #endif
 
+@interface AppDelegate ()
+
+@property (nonatomic, strong) NSDictionary *launchOptions;
+
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -30,33 +45,91 @@ static void InitializeFlipper(UIApplication *application) {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
+  
+  self.launchOptions = launchOptions;
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+   
+  #ifdef DEBUG
+      #if defined(EX_DEV_LAUNCHER_ENABLED)
+          EXDevLauncherController *controller = [EXDevLauncherController sharedInstance];
+          [controller startWithWindow:self.window delegate:(id<EXDevLauncherControllerDelegate>)self launchOptions:launchOptions];
+        #else
+          [self initializeReactNativeApp];
+        #endif
+    #else
+          [self initializeReactNativeApp];
+    #endif
+   
+    return YES;
+}
 
+- (RCTBridge *)initializeReactNativeApp
+{
+  #if defined(EX_DEV_LAUNCHER_ENABLED)
+    NSDictionary *launchOptions = [EXDevLauncherController.sharedInstance getLaunchOptions];
+  #else
+    NSDictionary *launchOptions = self.launchOptions;
+  #endif
+ 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"RN065RC4"
-                                            initialProperties:nil];
-
+   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"RN065RC4" initialProperties:nil];
+  
   if (@available(iOS 13.0, *)) {
       rootView.backgroundColor = [UIColor systemBackgroundColor];
   } else {
       rootView.backgroundColor = [UIColor whiteColor];
   }
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  return YES;
+  return bridge;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #if defined(EX_DEV_LAUNCHER_ENABLED)
+    return [[EXDevLauncherController sharedInstance] sourceUrl];
+  #else
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #endif
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
+// Linking API
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  #if defined(EX_DEV_LAUNCHER_ENABLED)
+  if ([EXDevLauncherController.sharedInstance onDeepLink:url options:options]) {
+    return true;
+  }
+  #endif
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+ 
+// Universal Links
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+}
+ 
 @end
+ 
+#if defined(EX_DEV_LAUNCHER_ENABLED)
+@implementation AppDelegate (EXDevLauncherControllerDelegate)
+ 
+- (void)devLauncherController:(EXDevLauncherController *)developmentClientController
+    didStartWithSuccess:(BOOL)success
+{
+  developmentClientController.appBridge = [self initializeReactNativeApp];
+}
+ 
+@end
+#endif
+
